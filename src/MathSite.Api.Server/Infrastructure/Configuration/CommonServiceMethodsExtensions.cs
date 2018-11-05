94dfe22c9311ba33252783118f91c5f1c85e5964
Andrey Mokeev <andrey@mokeev1995.ru>
2018-11-05 15:08:08 +0300

    register service methods with DI

---
+++ src/MathSite.Api.Server/Infrastructure/Configuration/CommonServiceMethodsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MathSite.Api.Server.Infrastructure.CommonServiceMethods;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MathSite.Api.Server.Infrastructure.Configuration
+{
+    public static class CommonServiceMethodsExtensions
+    {
+        public static IServiceCollection AddServiceMethods(this IServiceCollection services)
+        {
+            return services.AddScoped(typeof(AliasableServiceMethods<,>))
+                .AddScoped(typeof(CountableServiceMethods<,>))
+                .AddScoped(typeof(CrudServiceMethods<,>))
+                .AddScoped(typeof(PageableServiceMethods<,>));
+        }
+    }
+}
--- src/MathSite.Api.Server/Startup.cs
+++ src/MathSite.Api.Server/Startup.cs
@@ -56,6 +56,7 @@ namespace MathSite.Api.Server
             services.ConfigureDb(Configuration.GetConnectionString("Math"), isDevelopment);
             services.ConfigureDi(Configuration);
             services.ConfigureAuth(Configuration, isDevelopment);
+            services.AddServiceMethods();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
