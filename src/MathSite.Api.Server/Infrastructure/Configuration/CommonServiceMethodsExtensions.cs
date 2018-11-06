a2e4ab36863978502bee01327057e50a58effea2
Andrey Mokeev <andrey@mokeev1995.ru>
2018-11-06 03:02:39 +0300

    Was replaced concrete types registration with all types that is inherited from class

--- src/MathSite.Api.Server/Infrastructure/Configuration/CommonServiceMethodsExtensions.cs
+++ src/MathSite.Api.Server/Infrastructure/Configuration/CommonServiceMethodsExtensions.cs
@@ -1,4 +1,5 @@
-﻿using MathSite.Api.Server.Infrastructure.CommonServiceMethods;
+﻿using System.Linq;
+using MathSite.Api.Server.Infrastructure.CommonServiceMethods;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MathSite.Api.Server.Infrastructure.Configuration
@@ -7,10 +8,18 @@ namespace MathSite.Api.Server.Infrastructure.Configuration
     {
         public static IServiceCollection AddServiceMethods(this IServiceCollection services)
         {
-            return services.AddScoped(typeof(AliasableServiceMethods<,>))
-                .AddScoped(typeof(CountableServiceMethods<,>))
-                .AddScoped(typeof(CrudServiceMethods<,>))
-                .AddScoped(typeof(PageableServiceMethods<,>));
+            var baseEntityType = typeof(BaseEntityServiceMethods<,>);
+            var baseType = typeof(BaseServiceMethods);
+            var asm = baseType.Assembly;
+
+            var types = asm.GetTypes()
+                .Where(type => baseType.IsAssignableFrom(type)) // только наследники базовых
+                .Where(type => type != baseType && type != baseEntityType) // не базовые типы
+                .ToList();
+
+            types.ForEach(type => services.AddScoped(type));
+
+            return services;
         }
     }
 }
