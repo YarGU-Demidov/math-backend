cf291ae4715329f048a1639e02ae7f423757ede1
Selivanov Dmitry <d.selivanov17@yandex.ru>
2019-05-16 00:43:35 +0300

    enable cors

--- src/MathSite.Api.Server/Startup.cs
+++ src/MathSite.Api.Server/Startup.cs
@@ -62,7 +62,7 @@ namespace MathSite.Api.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment()) 
                 app.UseDevelopmentConfig();
             app.UseForwarding();
             app.UseAuthentication();
